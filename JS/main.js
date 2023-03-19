e316d7d0aeb97e545b78b762ec95e639052c30c9
Christian Langa <72701645+Andzani89@users.noreply.github.com>
2023-03-19 03:56:22 -0700

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+console.log("WELCOME TO FLIPIX STREAMING SERVICE");
+
+//Irequest button func
+function request(){
+    window.location.href = "./Irequest.html"; //this is the link
+}
+
+
---
+++ JS/stream.js
@@ -0,0 +1,26 @@
+console.log("START STREAMING")
+
+var video = document.getElementById('change');
+function epi1(){  //EPISODE NO.1
+    video.setAttribute('src', './trailer/All_of_Us_Are_Dead_Trailer_Resmi_Netflix_xVN-y9OaSag_135.mp4');
+}
+
+
+function ep2(){  //EPISODE NO.2
+    video.setAttribute('src', './Baymax.mp4');//YOUTUBE LINKS
+}
+
+function ep3(){  //EPISODE NO.3
+    video.setAttribute('src', './trailer/All_of_Us_Are_Dead_Trailer_Resmi_Netflix_xVN-y9OaSag_135.mp4');
+}
+
+
+
+
+
+
+
+
+
+
+
